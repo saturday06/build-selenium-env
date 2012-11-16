360d4df7aa7cb6b2b0df702e8cd2f58f8432eef4
Mou Damepo <you@moduamepo.moudamepo.moudamepoaos878e35ryqu>
2012-11-16 21:58:23 +0900

    aiosjdf

--- src/main/java/com/example/yami/PlaygroundTestCase.java
+++ src/main/java/com/example/yami/PlaygroundTestCase.java
@@ -3,23 +3,40 @@
  */
 package com.example.yami;
 
-import static org.junit.Assert.fail;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PlaygroundTestCase {
+	WebDriver wd;
+
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		if (wd != null) {
+			wd.close();
+		}
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws MalformedURLException {
+		DesiredCapabilities dc = DesiredCapabilities.firefox();
+		dc.setBrowserName("firefox");
+		wd = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+		wd.navigate().to("http://www.lunaport.net/");
+		wd.findElement(By.linkText("test")).click();
+		Assert.assertEquals("http://www.lunaport.net/test/", wd.getCurrentUrl());
 	}
 }
